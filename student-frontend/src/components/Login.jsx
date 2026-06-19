4a6ddee92811f6e7e6d90df8014aab315b6a0691
mithildhure <mithil.dhure1021@gmail.com>
2026-06-19 10:44:01 +0530

    no validation login page

--- student-frontend/src/components/Login.jsx
+++ student-frontend/src/components/Login.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+
+    const [user, setUser] = useState({
+        "username":'',
+        "password":''
+    });
+    const navigate = useNavigate();
+
+    const handleChange = (e)=>{
+        setUser({
+            ...user,[e.target.name] : e.target.value 
+        });
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        axios.post(`http://localhost:8080/login`, user).then((result) => {
+            navigate('/')
+        }).catch((err) => {
+            alert('Wrong Username or password');
+            console.log(err);
+        });
+    }
+
   return (
     <>
       
@@ -10,15 +35,17 @@ const Login = () => {
         className="container my-4 p-3 col-5 text-center bg-light rounded"
       >
             <h3 className='text-center p-3'>Login</h3>
-        <form >
+        <form onSubmit={handleSubmit}>
 
             <div className="form-floating mb-3">
                 <input
                     type="text"
                     className="form-control"
                     name="username"
+                    value={user.username}
                     id="uname"
                     placeholder=""
+                    onChange={handleChange}
                 />
                 <label htmlFor="uname">Username</label>
             </div>
@@ -28,8 +55,10 @@ const Login = () => {
                     type="password"
                     className="form-control"
                     name="password"
+                    value={user.password}
                     id="pass"
                     placeholder=""
+                    onChange={handleChange}
                 />
                 <label htmlFor="pass">Password</label>
             </div>  
